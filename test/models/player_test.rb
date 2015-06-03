244f44903b23663d526f2b3c393b0cb2cfda7360
annapolec <ania.polec@gmail.com>
2015-06-03 22:37:24 +0200

    Add unit test for Player

--- test/models/player_test.rb
+++ test/models/player_test.rb
@@ -1,7 +1,35 @@
 require 'test_helper'
 
 class PlayerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "player's atributes must not be empty" do
+  	player = Player.new
+  	assert player.invalid?
+  	assert player.errors[:first_name].any?
+  	assert player.errors[:last_name].any?
+  	assert player.errors[:avatar].any?  	
+  end
+  
+
+def new_player(image)
+	Player.new(first_name: "Ania",
+				last_name: "Polec",
+				avatar: image)
+end
+
+ test "avatar format" do
+	ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg
+			 http://a.b.c/x/y/z/fred.gif }
+	bad = %w{ fred.doc fred.gif/more fred.gif.more }
+	
+	ok.each do |name|
+		assert new_player(name).valid?, "#{name} should be valid"
+	end
+
+	bad.each do |name|
+		assert new_player(name).invalid?, "#{name} shouldn't be valid"
+	end
+  end
+
+
 end
