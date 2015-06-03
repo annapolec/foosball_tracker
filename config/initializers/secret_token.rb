e967bbe4453f3cd777c7ae3681d7d1b13fe47c70
annapolec <ania.polec@gmail.com>
2015-06-03 19:00:15 +0200

    App created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FoosballTracker::Application.config.secret_key_base = '1b32d5aca29bc1a193ac6fbede18a11f1038095a4b6eb8b43670c1b84701cbafbc4980a3207b0786208f164333a4e63962893cf7a82f759619636d3867545382'
