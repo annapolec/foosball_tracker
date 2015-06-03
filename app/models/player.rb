e72bb6016b55c3e530af4b8495afcf3b73eb3595
annapolec <ania.polec@gmail.com>
2015-06-03 22:40:05 +0200

    Add validations to Player

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,2 +1,8 @@
 class Player < ActiveRecord::Base
+	validates :first_name, :last_name, :avatar, presence: true
+ 	validates :avatar, allow_blank: true, format: {
+		with: %r{\.(gif|jpg|png)\Z}i,
+		message: 'avatar format must be GIF, JPG or PNG'
+	}
+
 end
