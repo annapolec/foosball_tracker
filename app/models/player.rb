2377399b8ef46b189d519fc013dafc804acb0621
annapolec <ania.polec@gmail.com>
2015-08-10 18:12:01 +0200

    style header, footer, forms

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,5 @@
-class Player < ActiveRecord::Base	
+class Player < ActiveRecord::Base
+	has_many :matches, dependent: :destroy	
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
