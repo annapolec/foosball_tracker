f38005c078826f6ff01263ef9e890c04b866809c
annapolec <ania.polec@gmail.com>
2015-08-18 21:53:37 +0200

    More configuration for Heroku

--- db/migrate/20150604185417_remove_avatar_column_from_players.rb
+++
@@ -1,9 +0,0 @@
-class RemoveAvatarColumnFromPlayers < ActiveRecord::Migration
-  def up
-  	remove_column :players, :avatar, :string
-  end
-
-  def down
-  	add_column :players, :avatar, :string
-  end
-end
